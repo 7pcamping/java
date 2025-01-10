a66dc9a389848415febf1a993fa0f1d3be04a8b7
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-10 21:19:11 +0530

    Create input from user.java

---
+++ input from user.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner=new Scanner(System.in);
+    int x=sc.nextInt();
+    System.out.prinltn(x);
+  }
+}
