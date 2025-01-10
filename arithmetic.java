d029b55d65fec89821888b9bf8cb9cb7a8b4231e
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-10 21:05:56 +0530

    Create arithmetic.java

---
+++ arithmetic.java
@@ -0,0 +1,11 @@
+class Main{
+  public static void main(String [] args){
+    int a=1;
+     int b =3;
+     System.out.println(a+b);
+     System.out.println(a-b);
+     System.out.prinltn(a*b);
+     System.out.println(a/b);
+    
+           
+
