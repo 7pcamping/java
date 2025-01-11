addeba213a966a0e028f8ce21f5304906c072476
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-11 21:44:01 +0530

    Create 2 variable &their sum.java

---
+++ 2 variable &their sum.java
@@ -0,0 +1,11 @@
+
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner=new Scanner(System.in);
+    int a=Scanner.nextInt();
+    int b=Scanner.nextInt();
+    int sum=(a+b);
+    System.out.println(sum);
+  }
+}
