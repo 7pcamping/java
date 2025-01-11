2d0809dc740fffd5c8ab88bea571e6ce1185b7d2
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-11 21:49:49 +0530

    Create two variable &their difference.java

---
+++ two variable &their difference.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner=new Scanner(System.in);
+    int a=Scanner.nextInt();
+    int b=Scanner.nextInt();
+    int diff=(a-b);
+    System.out.println(diff);
+    int mul=(a*b);
+    Sysyem.out.println(mul);
+    int div=(a/b);
+    System.out.println(div);
+    int mod=(a%b);
+    System.out.println(mod);
+    
