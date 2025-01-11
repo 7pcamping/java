582c0489f6a8453a89644997f464991c93e53189
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-11 21:54:55 +0530

    Create Conditional statement .java

---
+++ Conditional statement .java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner=new Scanner(System.in);
+    int age=sc.nextInt();
+    if(age>=18){
+      Systen.out.println("ADULT");
+    } else{
+      System.out.println("NOT ADULT")
+        }
+   }
+}
