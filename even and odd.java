7b6a708d2e7aeafdd3f49ec35c99459527843582
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-12 20:12:33 +0530

    Create even and odd.java

---
+++ even and odd.java
@@ -0,0 +1,11 @@
+import java .util.*;
+public class Main{
+  public static void main{
+    Scanner scanner=new Scanner(Syatem.in);
+    int x=sc.nextInt();
+    if(num%2==0){
+      System.out.println("Even No")
+    {else{
+        System.out.println("Odd No")
+          
+        
