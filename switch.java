60ff54e984a6a7ce33e9701c05d74133edfd4844
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-13 21:45:13 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,17 @@
+import .java.util.*;
+public class Main{
+  public static void main(String args[]){
+    //input
+    int button=1;
+  switch(button){
+    case 1:
+      System.out.println("hellow");
+      break;
+    case 2:
+      System.out.println("Namaste");
+      break;
+    case 3:
+      System.out.println("Bonjour");
+      break;
+      deafult:
+      System.out.println("Invaid);
