5c3d578f27eaa1a273c42ea826dbe836da649dcc
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-14 16:40:08 +0530

    Create while loop.java

---
+++ while loop.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class PrintNum{
+  public static void main(String args[]){
+    int i=0;
+    while(i<=10){
+      System.out.println(i);
+      i=i+1;
+    }
+  }
+}
+output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
