e91601343968b2aa8805da4700f943e7bbaab409
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-14 16:35:11 +0530

    Create loops.java

---
+++ loops.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class PrintNum{
+  public static void main(Strings args[]){
+    //implement the loops 0 to 10
+  for(int i=0;i<=10;i++){
+    System.out.println(i);
+    }
+  }
+}
+
+
+output:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
