367c452de8c20010636dec2fcb7c0a013419dde3
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-15 21:04:31 +0530

    Create loop from scanner class.java

---
+++ loop from scanner class.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Main{
+  public static void main(String args[]){
+    Scanner scanner=new Scanner(System.in){
+      int n=sc.nextInt();
+      int sum=0;
+      for(int i=0;i<=n;i++){
+      sum=sum+1;
+    }
+    System.out.println(sum);
+  }
+}
