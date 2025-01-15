92526df70fa090c3715fa6d86c1d56e5b509d836
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-15 20:59:07 +0530

    Create do while loop.java

---
+++ do while loop.java
@@ -0,0 +1,22 @@
+import java .util.*;
+public class Main{
+  public static void main(String args[]){
+    int i=0;
+    do{
+      System.out.println(i);
+      i=i+1;
+    }while(i<11);
+  }
+}
+
+// here is a output of a code//
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
