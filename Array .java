33d4da02df28880136f1e8588ea42ddeb9a0ba37
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-23 20:03:29 +0530

    Create Array .java

---
+++ Array .java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Array {
+    public static void main(String[] args) {
+        int [] marks=new int[3];
+        marks[0]=91;
+        marks[1]=98;
+        marks[2]=95;
+        //System.out.println(marks[0]);
+        for(int i=0;i<3;i++){
+            System.out.println(marks[i]);
+        }
+    }
+}
+
+OUTPUT:
+91
+98
+95
+
+Process finished with exit code 0
