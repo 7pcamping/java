946ed504e5e90a72ed852aa1fa1d69b6bc79322d
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-24 14:30:16 +0530

    Create without loop pattern.java

---
+++ without loop pattern.java
@@ -0,0 +1,11 @@
+public class java2 {
+    public static void main(String args[]){
+        System.out.print("*\n* *\n* * *\n* * * *");
+    }
+}
+OUTOUT:
+*
+* *
+* * *
+* * * *
+Process finished with exit code 0
