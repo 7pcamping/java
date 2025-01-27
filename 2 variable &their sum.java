6a3f2a0268f3e5e3fc378c955254f53b12850c3e
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-01-27 21:38:50 +0530

    Update 2 variable &their sum.java

--- 2 variable &their sum.java
+++ 2 variable &their sum.java
@@ -4,7 +4,9 @@ public class Main{
   public static void main(String args[]){
     Scanner scanner=new Scanner(System.in);
     int a=Scanner.nextInt();
+    System.out.println("Enter the number:");
     int b=Scanner.nextInt();
+    System.out.println("Enter the second numnber:");
     int sum=(a+b);
     System.out.println(sum);
   }
