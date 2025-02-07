8582d4c6fd2c3228b4024f4818dcc11f7552fb24
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-02-07 21:27:04 +0530

    Create linear search .java

---
+++ linear search .java
@@ -0,0 +1,24 @@
+public class LinearSearch {
+    // Method to perform linear search
+    public static int linearSearch(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i; // Return index if found
+            }
+        }
+        return -1; // Return -1 if key is not found
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 5, 8, 9, 10};
+        int key = 8;
+
+        int result = linearSearch(arr, key);
+        
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+}
