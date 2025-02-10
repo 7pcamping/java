1ec97ac8c23986076793f7d9f22353ddfa36266c
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-02-10 22:19:10 +0530

    Update linear search .java

--- linear search .java
+++ linear search .java
@@ -1,5 +1,5 @@
 public class LinearSearch {
-    // Method to perform linear search
+    
     public static int linearSearch(int[] arr, int key) {
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] == key) {
