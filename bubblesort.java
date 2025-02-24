d865bb94b7f61337fa5fbf13bc92c0d4f4683442
PRAGATI CHOUDHARY <choudharypragati46@gmail.com>
2025-02-24 21:23:22 +0530

    Create bubblesort.java

---
+++ bubblesort.java
@@ -0,0 +1,31 @@
+public class BubbleSort{
+
+    public static void printArray(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[]={7,8,3,1,2};
+        //bubble sort
+        //time complexity=0(n^2)
+        for(int i=0;i<arr.length-1;i++){
+            for(int j=0;j<arr.length-i-1;j++){
+                if(arr[j]>arr[j+1]){
+                    //swap
+                    int temp=arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
+            }
+        }
+        printArray(arr);
+    }
+
+}
+OUTOUT:
+1 
+2 
+3 
+7 
+8
